165ac337daa359681ff4323fb4c1d7c0add348f3
Shriroop <shriroop.joshi@gmail.com>
2017-12-07 00:23:56 -0600

    Initial commit

--- src/main/java/com/example/Driver.java
+++ src/main/java/com/example/Driver.java
@@ -1,28 +1,9 @@
-/*
- * Driver.java
- * 
- * Copyright 2014 Luca Menichetti <meniluca@gmail.com>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- */
-
 package com.example;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
@@ -37,6 +18,14 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
+
+import com.kennycason.kumo.CollisionMode;
+import com.kennycason.kumo.WordCloud;
+import com.kennycason.kumo.WordFrequency;
+import com.kennycason.kumo.bg.CircleBackground;
+import com.kennycason.kumo.font.scale.SqrtFontScalar;
+import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.palette.ColorPalette;
 
 public class Driver extends Configured implements Tool {
 
@@ -55,26 +44,24 @@ public class Driver extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
 
 		Configuration conf = new Configuration();
-	    Job job = Job.getInstance(conf, "Your job name");
-		
-	    job.setJarByClass(Driver.class);
-		
-		logger.info("job " + job.getJobName() + " [" + job.getJar()
-				+ "] started with the following arguments: "
+		Job job = Job.getInstance(conf, "Your job name");
+
+		job.setJarByClass(Driver.class);
+
+		logger.info("job " + job.getJobName() + " [" + job.getJar() + "] started with the following arguments: "
 				+ Arrays.toString(args));
 
 		if (args.length < 2) {
-			logger.warn("to run this jar are necessary at 2 parameters \""
-					+ job.getJar()
+			logger.warn("to run this jar are necessary at 2 parameters \"" + job.getJar()
 					+ " input_files output_directory");
 			return 1;
 		}
-		
+
 		job.setMapperClass(WordcountMapper.class);
 		logger.info("mapper class is " + job.getMapperClass());
-		
-		//job.setMapOutputKeyClass(Text.class);
-		//job.setMapOutputValueClass(IntWritable.class);
+
+		// job.setMapOutputKeyClass(Text.class);
+		// job.setMapOutputValueClass(IntWritable.class);
 		logger.info("mapper output key class is " + job.getMapOutputKeyClass());
 		logger.info("mapper output value class is " + job.getMapOutputValueClass());
 
@@ -82,9 +69,9 @@ public class Driver extends Configured implements Tool {
 		logger.info("reducer class is " + job.getReducerClass());
 		job.setCombinerClass(WordcountReducer.class);
 		logger.info("combiner class is " + job.getCombinerClass());
-		//When you are not runnign any Reducer
-		//OR 	job.setNumReduceTasks(0);
-		//		logger.info("number of reduce task is " + job.getNumReduceTasks());
+		// When you are not runnign any Reducer
+		// OR job.setNumReduceTasks(0);
+		// logger.info("number of reduce task is " + job.getNumReduceTasks());
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
@@ -98,14 +85,27 @@ public class Driver extends Configured implements Tool {
 		logger.info("output format class is " + job.getOutputFormatClass());
 
 		Path filePath = new Path(args[0]);
-		logger.info("input path "+ filePath);
+		logger.info("input path " + filePath);
 		FileInputFormat.setInputPaths(job, filePath);
 
 		Path outputPath = new Path(args[1]);
-		logger.info("output path "+ outputPath);
+		logger.info("output path " + outputPath);
 		FileOutputFormat.setOutputPath(job, outputPath);
 
 		job.waitForCompletion(true);
+
+		final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+		final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("/home/admin1/twitter/tweet-file");
+		final Dimension dimension = new Dimension(600, 600);
+		final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+		wordCloud.setPadding(2);
+		wordCloud.setBackground(new CircleBackground(300));
+		wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1),
+				new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
+		wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
+		wordCloud.build(wordFrequencies);
+		wordCloud.writeToFile("/home/admin1/twitter/output.png");
+
 		return 0;
 	}
 }
--- src/main/java/com/example/WordcountMapper.java
+++ src/main/java/com/example/WordcountMapper.java
@@ -1,25 +1,3 @@
-/*
- * WordcountMapper.java
- * 
- * Copyright 2014 Luca Menichetti <meniluca@gmail.com>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- */
-
 package com.example;
 
 import java.io.IOException;
@@ -34,8 +12,7 @@ public class WordcountMapper extends Mapper<LongWritable, Text, Text, IntWritabl
 	public static final IntWritable ONE = new IntWritable(1);
 
 	@Override
-	protected void map(LongWritable offset, Text line, Context context)
-			throws IOException, InterruptedException {
+	protected void map(LongWritable offset, Text line, Context context) throws IOException, InterruptedException {
 		for (String word : line.toString().split(" ")) {
 			context.write(new Text(word), ONE);
 		}
